b0d3ec29497679deec229b9d3fecd4722f612ed3
sho0712 <hqmhn857@yahoo.co.jp>
2022-02-12 14:11:34 +0900

    main.py Procfile作成 大沼アプリ参考

--- main.py
+++ main.py
@@ -47,9 +47,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-       line_bot_api.reply_message(event.reply_token,
-                              TextSendMessage(text=event.message.text))
-
+    line_bot_api.reply_message(event.reply_token,
+                               TextSendMessage(text="1111"))
 
 
 if __name__ == "__main__":
