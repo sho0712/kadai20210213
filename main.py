046b75d1246ec86c2e690f004871b65af2b19f9d
sho0712 <hqmhn857@yahoo.co.jp>
2022-02-12 15:34:39 +0900

    main.py Procfile作成 大沼アプリ参考

--- main.py
+++ main.py
@@ -50,8 +50,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     a = pandas_1.main
+    b = f'{a}です'
     line_bot_api.reply_message(event.reply_token,
-                               TextSendMessage(text=a))
+                               TextSendMessage(text=b))
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
